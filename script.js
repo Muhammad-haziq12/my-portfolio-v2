c7dd3b1baede7bf3f1a34782e48a2a6259880b63
Muhammad-Haziq12 <muhammad.haziq@student.vinci.be>
2025-10-31 00:12:35 +0100

    h2

--- script.js
+++ script.js
@@ -17,22 +17,3 @@
     }
 })();
 // end: Navbar
-
-// start: Smooth scroll for navigation links
-(function () {
-    const navLinks = document.querySelectorAll(".navbar-menu-item-link");
-    navLinks.forEach(function (link) {
-        link.addEventListener("click", function (e) {
-            const href = this.getAttribute("href");
-            if (href.startsWith("#") && href !== "#") {
-                e.preventDefault();
-                const target = document.querySelector(href);
-                if (target) {
-                    target.scrollIntoView({ behavior: "smooth" });
-                    document.querySelector("[data-navbar]").classList.remove("active");
-                }
-            }
-        });
-    });
-})();
-// end: Smooth scroll for navigation links
